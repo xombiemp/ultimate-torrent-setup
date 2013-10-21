7e9cd5d9d32c7242f5adcd4e478343c873bc3cae
Andrew Parker <andrewmichaelparker@gmail.com>
2013-10-21 10:04:58 -0600

    Initial Commit

---
+++ configs/site/js/application.js
@@ -0,0 +1,23 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+
+    $('.button-loading')
+      .click(function () {
+        var btn = $(this)
+        btn.button('loading')
+        setTimeout(function () {
+          btn.button('reset')
+        }, 3000)
+      })
+  })
+
+// Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
+
+}(window.jQuery)
